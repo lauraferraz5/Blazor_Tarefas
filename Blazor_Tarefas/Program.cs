73d309c5539c10010a96772310fd932605cb2803
Laura Ferraz <laura@magnetcustomer.com>
2021-12-01 11:39:01 -0300

    aicao de repositorio

--- Blazor_Tarefas/Program.cs
+++ Blazor_Tarefas/Program.cs
@@ -1,3 +1,4 @@
+using Blazor_Tarefas.Repositorios;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +18,9 @@ namespace Blazor_Tarefas
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<IRepositorio, Repositorio>();
 
             await builder.Build().RunAsync();
         }
